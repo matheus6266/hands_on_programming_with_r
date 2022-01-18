0be2b3f1060e108c075c4700a47701edd5cd8790
matheus6266 <matheusbarros.o@outlook.com>
2022-01-18 11:36:52 -0300

    Versao 2

---
+++ if_condition.R
@@ -0,0 +1,12 @@
+
+if (same_symbols){
+  prize <- # look up the prize
+} else if (bars){
+  prize <- #assign $5
+} else {
+  # count cherries
+  prize <- #calculate a prize
+}
+
+# count diamonds
+# double the prize if necessary
